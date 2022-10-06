edfe3d515ac4ca9eac600637893094c08355daf4
RicardoYone <xrikardox19@hotmail.com>
2022-10-05 22:42:21 -0500

    final proyecto

--- src/controllers/candidatos.controller.js
+++ src/controllers/candidatos.controller.js
@@ -7,13 +7,33 @@ export const crearCandidato=async(req,res)=>{
 
     try{
         //Con estos await hacemos la validacion de que existan esos partidosPoliticos y region respectivamente.
-        await conexion.partidoPolitico.findUniqueOrThrow({
+        const partidoPolitico= await conexion.partidoPolitico.findUniqueOrThrow({
             where:{id:data.partidoPoliticoId},
+            select:{
+                nombre:true,
+            }
         });
 
-        await conexion.region.findFirstOrThrow({
+        //SELECT nombre FROM regiones WHERE id=...;
+        const region=await conexion.region.findFirstOrThrow({
             where:{id:data.regionId},
+            select:{
+                nombre:true,
+            }
         });
+
+        //SELECT * FROM candidato WHERE partido_politico_id=... AND region_id=...; LIMIT 1;
+        const candidato = await conexion.candidato.findFirst({
+            where:{
+                partidoPoliticoId:data.partidoPoliticoId,
+                regionId:data.regionId,
+            },
+        });
+
+        if(candidato!==null){
+            throw new Error(`El candidato por el partido ${partidoPolitico.nombre} por la region ${region.nombre} ya existe`)
+        }
+
 
         const resultado=await conexion.candidato.create({
             data,
@@ -31,15 +51,39 @@ export const crearCandidato=async(req,res)=>{
                 content:error.message
             });
         }
+
+        return res.status(400).json({
+            message:"Error desconocido",
+            content:error.message
+        })
     }
 
-    return res.status(400).json({
-        message:"Error desconocido",
-    })
-
+    
 };
 
+export const listarCandidatos=async(req,res)=>{
+    const{regionId,partidoPoliticoId}=req.query; //Este sirve para pasarle los parametros de query y string ?nombre?'Luis&region=Arequipa&apellido=Martines 
 
+    let where={};
+
+    if(regionId){
+        console.log("hay regionId");
+        where={...where,regionId: parseInt(regionId)};
+    }
+
+    if(partidoPoliticoId){
+        console.log("Hay partidoPoliticoId");
+        where={...where,partidoPoliticoId: +partidoPoliticoId};
+    }
+
+    const candidatos=await conexion.candidato.findMany({
+        where,
+    });
+
+    return res.json({
+        candidatos,
+    })
+};
 
 
 
