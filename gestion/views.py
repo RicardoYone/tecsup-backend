67ed18141e616ed9071a9f2b3929db9a213fbf58
RicardoYone <xrikardox19@hotmail.com>
2023-02-07 15:03:20 -0500

    final

--- gestion/views.py
+++ gestion/views.py
@@ -1,18 +1,18 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.request import Request
-from rest_framework.generics import ( ListAPIView, 
-                                      ListCreateAPIView, 
-                                      RetrieveUpdateDestroyAPIView, 
-                                      CreateAPIView )
-from .serializers import (  PruebaSerializer, 
-                            ImportanciaSerializer, 
-                            ImportanciaSerializerRUD, 
-                            TareaSerializer, 
-                            TareaConImportanciaSerializer, 
-                            EtiquetaSerializer,
-                            TareaEtiquetaSerializer
-                            )
+from rest_framework.generics import (ListAPIView,
+                                     ListCreateAPIView,
+                                     RetrieveUpdateDestroyAPIView,
+                                     CreateAPIView)
+from .serializers import (PruebaSerializer,
+                          ImportanciaSerializer,
+                          ImportanciaSerializerRUD,
+                          TareaSerializer,
+                          TareaConImportanciaSerializer,
+                          EtiquetaSerializer,
+                          TareaEtiquetaSerializer
+                          )
 from .models import Importancia, Tarea, Etiqueta, TareaEtiqueta
 # con esto podemos utilizar la conexion actual de nuestro proyecto a la bd, sin la necesidad de crear una nueva conexion y rebalsar el numero de conexiones maximas permitidas
 from django.db import connection
@@ -21,6 +21,8 @@ from rest_framework import status
 from pytz import utc
 
 # Tipado > indicar a determinadas variables su tipo de dato correspondiente
+
+
 @api_view(http_method_names=['GET', 'POST'])
 def endpointInicial(request: Request):
     print(request.method)
@@ -34,20 +36,21 @@ def endpointInicial(request: Request):
         print(request.data)
 
         return Response(data={
-            'message':'Se creo la informacion correctamente'
+            'message': 'Se creo la informacion correctamente'
         })
+
 
 class PruebaApiView(ListCreateAPIView):
     # 2 atributos : queryset y serializer_class
     queryset = [
         {
-            'id': 1, 
-            'nombre': 'eduardo', 
+            'id': 1,
+            'nombre': 'eduardo',
             'apellido': 'de rivero'
-        }, 
+        },
         {
-            'id': 2, 
-            'nombre': 'fiorella', 
+            'id': 2,
+            'nombre': 'fiorella',
             'apellido': 'marquez'
         }]
     serializer_class = PruebaSerializer
@@ -72,40 +75,43 @@ class PruebaApiView(ListCreateAPIView):
                 'content': data.errors
             }, status=400)
 
+
 class ImportanciasView(ListCreateAPIView):
-    queryset = Importancia.objects.all() # SELECT * FROM importancia;
+    queryset = Importancia.objects.all()  # SELECT * FROM importancia;
     serializer_class = ImportanciaSerializer
-    
+
     def get_queryset(self):
         # SELECT * FROM importancias WHERE delete = false;
         return self.queryset.filter(deleted=False).all()
 
     def get(self, request: Request):
-        instancias = self.get_queryset() 
+        instancias = self.get_queryset()
         # print(instancias[0].nombre)
         for instancia in instancias:
             print(instancia.nombre)
         # cuando tenemos ya la informacion de la base de datos entonces seran instancias (registros), en cambio cuando tenemos la informacion a guardar en la bd y queremos validar que este correcta lo pasaremos mediante data, instance espera una o varias instancias, data espera informacion
-        data_serializada = self.serializer_class(instance= instancias, many=True)
+        data_serializada = self.serializer_class(
+            instance=instancias, many=True)
 
-        return Response ({
+        return Response({
             'message': 'Las instancias son',
             'content': data_serializada.data
         })
-    
+
     def post(self, request: Request):
-        informacion = request.data # Informacion extraida del body
+        informacion = request.data  # Informacion extraida del body
         dataASerializar = self.serializer_class(data=informacion)
 
         dataASerializar.is_valid(raise_exception=True)
 
-        nuevaImportancia = dataASerializar.save() # guarda la informacion en la base de datos
+        # guarda la informacion en la base de datos
+        nuevaImportancia = dataASerializar.save()
 
         ###
 
         # # Primera forma de guardado de la informacion en la base de datos usando el modelo directamente
         # infoImportancia = {'nombre':'Ejemplo'}
-        # Importancia.objects.create(**infoImportancia) 
+        # Importancia.objects.create(**infoImportancia)
 
         # # Segunda forma
         # nuevaImportancia = Importancia(**infoImportancia)
@@ -114,7 +120,7 @@ class ImportanciasView(ListCreateAPIView):
         ###
 
         return Response({
-            'message':'Importancia creada exitosamente',
+            'message': 'Importancia creada exitosamente',
             'content': self.serializer_class(instance=nuevaImportancia).data
         }, status=201)
 
@@ -122,17 +128,17 @@ class ImportanciasView(ListCreateAPIView):
 # GET, PUT, DELETE
 class ImportanciaView(RetrieveUpdateDestroyAPIView):
     queryset = Importancia.objects.all()
-    serializer_class=ImportanciaSerializerRUD
+    serializer_class = ImportanciaSerializerRUD
 
     def validarImportancia(self, pk):
         # SELECT * FROM importancias WHERE id = ... and deleted = false LIMIT 1;
-        resultado = Importancia.objects.filter(id=pk, deleted= False).first()
+        resultado = Importancia.objects.filter(id=pk, deleted=False).first()
 
         if resultado is None:
             return Response(data={
                 'message': 'Importancia no existe',
                 'content': None
-            }, status = 404)
+            }, status=404)
         return resultado
 
     def get(self, request, pk):
@@ -150,7 +156,7 @@ class ImportanciaView(RetrieveUpdateDestroyAPIView):
                 'message': None,
                 'content': dataSerializada
             })
-    
+
     def delete(self, request, pk):
         resultado = self.validarImportancia(pk)
         if isinstance(resultado, Response):
@@ -162,7 +168,7 @@ class ImportanciaView(RetrieveUpdateDestroyAPIView):
                 'message': 'Importancia eliminada exitosamente',
                 'content': None
             })
-    
+
     def update(self, request, pk):
         resultado = self.validarImportancia(pk)
 
@@ -178,13 +184,16 @@ class ImportanciaView(RetrieveUpdateDestroyAPIView):
 
             # aca se actualiza la informacion de la importancia
             # retorna la instancia actualizada
-            importanciaActualizada = dataSerializada.update(resultado, dataSerializada.validated_data)
-            
+            importanciaActualizada = dataSerializada.update(
+                resultado, dataSerializada.validated_data)
+
             # retornamos la importacia actualizada
             return Response(data={
                 'message': 'Importancia actualizada exitosamente',
-                'content': self.serializer_class(instance=importanciaActualizada).data  # dataSerializada.data # va a ser la informacion que he pasado al momento de llamar al serializador  
+                # dataSerializada.data # va a ser la informacion que he pasado al momento de llamar al serializador
+                'content': self.serializer_class(instance=importanciaActualizada).data
             })
+
 
 class TareasView(ListCreateAPIView):
     queryset = Tarea.objects.all()
@@ -192,8 +201,8 @@ class TareasView(ListCreateAPIView):
 
     def post(self, request: Request):
         dataSerializada = self.serializer_class(data=request.data)
-        
         dataSerializada.is_valid(raise_exception=True)
+        
         # validar si esa importancia 'existe' (deleted  no es True )
         # al momento de hacer la validacion del serializador tbn hace la busqueda de las llaves foraneas, es decir, valida que esas FK existan en la bd, es por ello que no tenemos que volver a hacer la busque de la importancia en la bd.
         importancia = dataSerializada.validated_data.get('importancia')
@@ -202,7 +211,7 @@ class TareasView(ListCreateAPIView):
         if importancia.deleted == True:
             return Response(data={
                 'message': 'Importancia no existe'
-            }, status =400)
+            }, status=400)
 
         fechaHoy = utc.localize(datetime.now().utcnow())
         fechaCaducidad = dataSerializada.validated_data.get('fechaCaducidad')
@@ -214,19 +223,19 @@ class TareasView(ListCreateAPIView):
 
         nuevaTarea = dataSerializada.save()
 
-
         return Response(data={
             'message': 'Tarea creada exitosamente',
             'content': self.serializer_class(instance=nuevaTarea).data
 
-        }, status= status.HTTP_201_CREATED)
-    
+        }, status=status.HTTP_201_CREATED)
+
     def get(self, request: Request):
         tareas = self.get_queryset()
-        dataSerializada = TareaConImportanciaSerializer(instance=tareas, many=True)
+        dataSerializada = TareaConImportanciaSerializer(
+            instance=tareas, many=True)
         # El lenguaje SQL utilizado desde tarea para devolver sus importancias y sus etiquetas, seria el siguiente:
-        # SELECT * 
-        # FROM tareas as T 
+        # SELECT *
+        # FROM tareas as T
         # 	INNER JOIN tareas_etiquetas AS TE on T.id = TE.tarea_id
         #     INNER JOIN etiquetas as E on TE.etiqueta_id = E.id
         #     INNER JOIN importancias as I on T.importancia_id = I.id;
@@ -235,13 +244,16 @@ class TareasView(ListCreateAPIView):
             'content': dataSerializada.data
         })
 
+
 class EtiquetasView(ListCreateAPIView):
     queryset = Etiqueta.objects.all()
     serializer_class = EtiquetaSerializer
 
+
 class TareaEtiquetasView(CreateAPIView):
     queryset = TareaEtiqueta.objects.all()
     serializer_class = TareaEtiquetaSerializer
+
 
 @api_view(http_method_names=['GET'])
 def usandoVista(request):
@@ -250,7 +262,8 @@ def usandoVista(request):
     cursor = connection.cursor()
     # ejecutaremos una query sin utilizar los modelos definidos
     # Con parametros
-    cursor.execute('SELECT * FROM listar_etiquetas_con_la_letra_A where id = %s and deleted= %s', [4,True])
+    cursor.execute(
+        'SELECT * FROM listar_etiquetas_con_la_letra_A where id = %s and deleted= %s', [4, True])
     # Sin parametros
     # cursor.execute('SELECT * FROM listar_etiquetas_con_la_letra_A where')
 
@@ -269,5 +282,5 @@ def usandoVista(request):
         print(diccionario)
 
     return Response(data={
-        'message':'Se uso la vista'
+        'message': 'Se uso la vista'
     })
